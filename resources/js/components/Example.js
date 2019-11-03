c21a563d64d35987726ef834154816ecb3cc87f0
Sourav R <developer.srv1@gmail.com>
2019-11-03 19:50:52 +0530

    smooth dnd test implement

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Draggable } from 'react-smooth-dnd';
+import { applyDrag, generateItems } from './utils';
 
-function Example() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+const groupStyle = {
+  marginLeft: '50px',
+  flex: 1
 }
 
-export default Example;
+class Copy extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      items1: [
+        { id: '1' + 1, data: `Source Draggable - 1` },
+        { id: '1' + 2, data: `Source Draggable - 2` }
+      ],
+      items2: [
+        { id: '2' + 1, data: `Draggable 2 - 1` },
+        { id: '2' + 2, data: `Draggable 2 - 2` },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'stretch', marginTop: '50px', marginRight: '50px' }}>
+        <div style={groupStyle}>
+          <Container groupName="1" behaviour="copy" getChildPayload={i => this.state.items1[i]} onDrop={e => this.setState({ items1: applyDrag(this.state.items1, e) })}>
+            {
+              this.state.items1.map((p,i) => {
+                return (
+                  <Draggable key={i}>
+                    <div className="draggable-item">
+                      {p.data}
+                    </div>
+                  </Draggable>
+                );
+              })
+            }
+          </Container>
+        </div>
+        <div style={groupStyle}>
+          <Container groupName="1" lockAxis="y" getChildPayload={i => this.state.items2[i]} onDrop={e => this.setState({ items2: applyDrag(this.state.items2, e) })}>
+            {
+              this.state.items2.map((p, i) => {
+                return (
+                  <Draggable key={i}>
+                    <div className="draggable-item">
+                      {p.data}
+                    </div>
+                  </Draggable>
+                );
+              })
+            }
+          </Container>
+        </div>             
+      </div>
+    );
+  }
+}
+
+
+export default Copy;
 
 if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+    ReactDOM.render(<Copy />, document.getElementById('example'));
 }
--- resources/views/builder.blade.php
+++ resources/views/builder.blade.php
@@ -20,7 +20,7 @@
             <div class="col">
                 <div class="card bg-light mb-3">
                     <div class="card-header">Header</div>
-                    <div class="card-body">
+                    <div class="card-body" id="example">
                         <h5 class="card-title">Light card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
