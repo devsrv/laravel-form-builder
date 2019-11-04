ab7b1b36473931b34c6764bd81192143c1979e2a
Sourav R <developer.srv1@gmail.com>
2019-11-04 23:15:19 +0530

    date field label typo

--- resources/js/components/FormBuilder/pallet.js
+++ resources/js/components/FormBuilder/pallet.js
@@ -10,7 +10,7 @@ export default class Pallet extends Component {
 		this.state = {
 			fields: [
 				{ id: 1, type: 'input', label: 'Input field', isRequired: false, additionalConfig: {inputType: 'text'} },
-				{ id: 2, type: 'date', label: 'Date field', isRequired: false, additionalConfig: {} },
+				{ id: 2, type: 'date', label: 'Datepicker', isRequired: false, additionalConfig: {} },
 				{ id: 3, type: 'select', label: 'Drop-down list', isRequired: false, additionalConfig: {listOptions: []} },
 				{ id: 4, type: 'textarea', label: 'Textarea', isRequired: false, additionalConfig: {textAreaRows: 3} }
 			]
