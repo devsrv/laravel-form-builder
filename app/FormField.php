bd13bac6f9f4fa546c293df33bf9eeea791f6da5
Sourav R <developer.srv1@gmail.com>
2019-11-05 17:22:36 +0530

    field options attribute json cast & pivot relation

--- app/FormField.php
+++ app/FormField.php
@@ -9,4 +9,15 @@ class FormField extends Model
     public $timestamps = false;
     
     protected $guarded = [];
+
+    /**
+     * Get the field's option.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getOptionsAttribute($value)
+    {
+        return json_decode($value);
+    }
 }
