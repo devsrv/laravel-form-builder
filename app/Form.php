6a14aa5f684390b9ad72b0348b41f667db2d4dc6
Sourav R <developer.srv1@gmail.com>
2019-11-05 15:34:35 +0530

    models created & eloquent relations defined

---
+++ app/Form.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Form extends Model
+{
+    protected $guarded = [];
+
+    /**
+     * The fields that belong to the form.
+     */
+    public function fields()
+    {
+        return $this->belongsToMany('App\Field', 'form_fields');
+    }
+}
