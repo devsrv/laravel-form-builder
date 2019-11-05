009ff15997cc55298029b3ead338e9f64270264e
Sourav R <developer.srv1@gmail.com>
2019-11-05 20:39:00 +0530

    validation error data show

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -8,6 +8,12 @@
     <div class="section mb-5">
         <div class="row">
             <div class="col-8 offset-2">
+                @if($errors->any())
+                    <div class="alert alert-danger" role="alert">
+                        <strong>Error!</strong> validation error occurred
+                    </div>
+                @endif
+
                 <div class="card bg-light mb-3">
                     <div class="card-body">
                         <form action="{{ route('submit.form') }}" method="post">
@@ -29,55 +35,29 @@
 
                                     @switch($field->field_type)
                                         @case("select")
-                                            <select id="{{ $id_for }}" name={{ $field_name }} class="custom-select">
-                                                <option value="0" selected>Choose...</option>
+                                            <select id="{{ $id_for }}" name={{ $field_name }} class="custom-select @error($field_name) is-invalid @enderror">
+                                                <option value="0">Choose...</option>
                                                 @foreach(explode(",", $options->values) as $value)
-                                                <option value="{{ $value }}">{{ $value }}</option>
+                                                <option value="{{ trim($value) }}" {{ old($field_name) == trim($value)? "selected" : "" }}>{{ trim($value) }}</option>
                                                 @endforeach
                                             </select>
                                             @break
 
                                         @case("textarea")
-                                            <textarea class="form-control" id="{{ $id_for }}" name={{ $field_name }} rows={{ $options->rows }}></textarea>
+                                            <textarea class="form-control @error($field_name) is-invalid @enderror" id="{{ $id_for }}" name={{ $field_name }} rows={{ $options->rows }}>{{ old($field_name) }}</textarea>
                                             @break
 
                                         @default
-                                            <input type="{{ $options->type }}" class="form-control" name={{ $field_name }} id="{{ $id_for }}" />
-                                            
+                                            <input type="{{ $options->type }}" class="form-control @error($field_name) is-invalid @enderror" name={{ $field_name }} id="{{ $id_for }}" value="{{ old($field_name) }}" />
                                     @endswitch
+                                
+                                    @error($field_name)
+                                        <div class="invalid-feedback">
+                                            {{ $errors->first($field_name) }}
+                                        </div>
+                                    @enderror
                                 </div>
                             @endforeach
-
-                        {{--
-                            <div class="form-group">
-                                <label for="exampleInputEmail2">Email address</label>
-                                <input type="email" class="form-control is-valid" id="exampleInputEmail2" aria-describedby="emailHelp" placeholder="Enter email">
-                                <div class="valid-feedback">
-                                    Looks good!
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail3">Email address</label>
-                                <input type="email" class="form-control is-invalid" id="exampleInputEmail3" aria-describedby="emailHelp" placeholder="Enter email">
-                                <div class="invalid-feedback">
-                                    Please provide a valid city.
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputState">State</label>
-                                <select id="inputState" class="custom-select is-invalid">
-                                    <option selected>Choose...</option>
-                                    <option>...</option>
-                                </select>
-                                <div class="invalid-feedback">Example invalid custom select feedback</div>
-                            </div>
-                            <div class="form-group">
-                                <label for="validationTextarea">Textarea</label>
-                                <textarea class="form-control is-invalid" id="validationTextarea" placeholder="Required example textarea" required></textarea>
-                                <div class="invalid-feedback">
-                                    Please enter a message in the textarea.
-                                </div>
-                            </div> --}}
 
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
