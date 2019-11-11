4a1885513279e2541a72f9c2930a6aa5ff554793
sourav r <developer.srv1@gmail.com>
2019-11-11 16:08:37 +0530

    hide the form submit button when there is no field

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -22,55 +22,57 @@
 
                 <div class="card bg-light mb-3">
                     <div class="card-body">
-                        <form action="{{ route('submit.form') }}" method="post">
-                            @csrf
-                            <input type="hidden" name="form_id" value="{{ $form_id }}" />
-                            <input type="hidden" name="field_ids" value="{{ $field_ids }}" />
+                        @if(count($fields) > 0)
+                            <form action="{{ route('submit.form') }}" method="post">
+                                @csrf
+                                <input type="hidden" name="form_id" value="{{ $form_id }}" />
+                                <input type="hidden" name="field_ids" value="{{ $field_ids }}" />
 
-                            @foreach($fields as $field)
-                                @php
-                                    $options = $field->pivot->options? json_decode($field->pivot->options) : null;
-                                    $field_name = 'field_' . $field->pivot->id;
-                                    $id_for = 'input-fld-'. $loop->iteration;
-                                @endphp
+                                @foreach($fields as $field)
+                                    @php
+                                        $options = $field->pivot->options? json_decode($field->pivot->options) : null;
+                                        $field_name = 'field_' . $field->pivot->id;
+                                        $id_for = 'input-fld-'. $loop->iteration;
+                                    @endphp
 
-                                <div class="form-group">
-                                    @if($options->label)
-                                        <label for="{{ $id_for }}">{{ $options->label }}</label>
-                                    @endif
+                                    <div class="form-group">
+                                        @if($options->label)
+                                            <label for="{{ $id_for }}">{{ $options->label }}</label>
+                                        @endif
 
-                                    @switch($field->field_type)
-                                        @case("select")
-                                            <select id="{{ $id_for }}" name={{ $field_name }} class="custom-select @error($field_name) is-invalid @enderror">
-                                                <option value="">Choose...</option>
-                                                @foreach(explode(",", $options->values) as $value)
-                                                <option value="{{ trim($value) }}" {{ old($field_name) == trim($value)? "selected" : "" }}>{{ trim($value) }}</option>
-                                                @endforeach
-                                            </select>
-                                            @break
+                                        @switch($field->field_type)
+                                            @case("select")
+                                                <select id="{{ $id_for }}" name={{ $field_name }} class="custom-select @error($field_name) is-invalid @enderror">
+                                                    <option value="">Choose...</option>
+                                                    @foreach(explode(",", $options->values) as $value)
+                                                    <option value="{{ trim($value) }}" {{ old($field_name) == trim($value)? "selected" : "" }}>{{ trim($value) }}</option>
+                                                    @endforeach
+                                                </select>
+                                                @break
 
-                                        @case("textarea")
-                                            <textarea class="form-control @error($field_name) is-invalid @enderror" id="{{ $id_for }}" name={{ $field_name }} rows={{ $options->rows }}>{{ old($field_name) }}</textarea>
-                                            @break
+                                            @case("textarea")
+                                                <textarea class="form-control @error($field_name) is-invalid @enderror" id="{{ $id_for }}" name={{ $field_name }} rows={{ $options->rows }}>{{ old($field_name) }}</textarea>
+                                                @break
 
-                                        @default
-                                            <input type="{{ $options->type == "date" ? "text" : $options->type }}" class="form-control {{ $options->type == "date"? "datepicker" : "" }} @error($field_name) is-invalid @enderror" name={{ $field_name }} id="{{ $id_for }}" value="{{ old($field_name) }}" />
-                                    @endswitch
+                                            @default
+                                                <input type="{{ $options->type == "date" ? "text" : $options->type }}" class="form-control {{ $options->type == "date"? "datepicker" : "" }} @error($field_name) is-invalid @enderror" name={{ $field_name }} id="{{ $id_for }}" value="{{ old($field_name) }}" />
+                                        @endswitch
 
-                                    @error($field_name)
-                                        <div class="invalid-feedback">
-                                            {{ $errors->first($field_name) }}
-                                        </div>
-                                    @enderror
+                                        @error($field_name)
+                                            <div class="invalid-feedback">
+                                                {{ $errors->first($field_name) }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                @endforeach
+
+                                <div class="form-group mt-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fa fa-paper-plane-o" aria-hidden="true"></i> Submit
+                                    </button>
                                 </div>
-                            @endforeach
-
-                            <div class="form-group mt-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-paper-plane-o" aria-hidden="true"></i> Submit
-                                </button>
-                            </div>
-                        </form>
+                            </form>
+                        @endif
                     </div>
                 </div>
             </div>
